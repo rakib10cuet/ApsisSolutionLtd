d8972f85260a01e27613c7b34b5f16751f8ffeca
Rezwoan Ahmed Rakib <74377574+rakib10cuet@users.noreply.github.com>
2021-07-13 01:44:29 +0600

    Update PasswordValidationRules.php
    After solving writer password pattern

--- ApsisSolutionLtd/app/Actions/Fortify/PasswordValidationRules.php
+++ ApsisSolutionLtd/app/Actions/Fortify/PasswordValidationRules.php
@@ -14,7 +14,7 @@ trait PasswordValidationRules{
         if($uType == 1){
             return ['required', 'string', 'regex:/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^A-Za-z0-9@]).{12,}$/','confirmed'];
         }elseif ($uType == 2){
-            return ['required', 'string', 'regex:/^(?=.*?[A-Za-z])(?=.*?[0-9])(?=.*?[^A-Za-z0-9]).{8,}$/','confirmed'];
+            return ['required', 'string', 'regex:/^(?=[^A-Z]*[A-Z])(?=[^a-z]*[a-z])(?=.*?[0-9])(?=.*?[^A-Za-z0-9@])(?!.*[pP]ass|.*[qQ]wer)(?!.*(.).*\1.*\1).{8,}$/','confirmed'];
         }else{
             return ['required', 'string', 'regex:/^(?=.*?[A-Za-z])(?=.*?[0-9]).{6,16}$/', 'confirmed'];
         }
